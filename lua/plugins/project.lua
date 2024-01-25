0fa87bd68977076a9c33d089367ddd78b6dd2c77
Jan van Hest <jan.vanhest@gmail.com>
2024-01-25 02:06:21 +0100

    UPD project.lua

--- lua/plugins/project.lua
+++ lua/plugins/project.lua
@@ -1,21 +1,25 @@
 return {
 
-  "ahmedkhalf/project.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  keys = {
-    { "<leader>sp", "<cmd>Telescope projects<cr>", desc = "Projects" },
-  },
-  opts = {
-    patterns = {
-    ".git",
-    "_darcs",
-    "Makefile",
-    "package.json",
-    "pyproject.toml",
-    ">workspace" },
-  },
-  config = function(_, opts)
-    require("project_nvim").setup(opts)
-    require('telescope').load_extension('projects')
-  end,
+    "ahmedkhalf/project.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    keys = {
+        { "<leader>sp", "<cmd>Telescope projects<cr>", desc = "Projects" },
+    },
+    config = function()
+        require("project_nvim").setup {
+            detection_methods = { "pattern" },
+            patterns = {
+                ".git",
+                "_darcs",
+                "Makefile",
+                "package.json",
+                "pyproject.toml",
+                ">workspace",
+            },
+            show_hidden = true,
+            silent_chdir = false,
+            datapath = vim.fn.stdpath "data",
+        }
+        require("telescope").load_extension "projects"
+    end,
 }
